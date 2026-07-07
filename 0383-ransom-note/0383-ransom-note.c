6be6914b8845016bf98a019c6ad516bb215ac1c6
Ramesh Pandhare <rameshpandhare347@gmail.com>
2026-07-07 16:59:05 +0530

    Time: 8 ms (14.64%), Space: 9.5 MB (68.99%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.c
@@ -0,0 +1,14 @@
+bool canConstruct(char* ransomNote, char* magazine) 
+{
+    int arr[26]={0};
+    for(int i=0; magazine[i]!='\0'; i++)
+    arr[magazine[i] - 'a']++;
+
+    for(int i=0; ransomNote[i]!='\0'; i++)
+    {
+        if(arr[ransomNote[i]-'a'] == 0) 
+        return false;
+        else arr[ransomNote[i]-'a']--;
+    }
+    return true;
+}
