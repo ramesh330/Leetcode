2359998f8f64b05e44683572616acaf4cd23e4c8
Ramesh Pandhare <rameshpandhare347@gmail.com>
2026-07-13 15:19:25 +0530

    Time: 0 ms (100%), Space: 11.1 MB (56.17%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.c
+++ 0013-roman-to-integer/0013-roman-to-integer.c
@@ -1,28 +1,30 @@
-int roman(char c){
-    switch(c){
-        case 'I' : return 1;
-        case 'V' : return 5;
-        case 'X' : return 10;
-        case 'L' : return 50;
-        case 'C' : return 100;
-        case 'D' : return 500;
-        case 'M' : return 1000;
+int romanVal(char c)
+{
+    switch(c)
+    {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
         default : return 0;
     }
 }
 
 int romanToInt(char* s) {
-    int curr=0,next=0,res=0;
-    int i=0;
-    while(s[i]){
-        curr = roman(s[i]);
-        next = roman(s[i+1]);
-        if(curr>=next)
-        res += curr;
-        else 
-        res -= curr;
+    int i=0,result=0;
+    while(s[i])
+    {
+        int curr = romanVal(s[i]);
+        int next = romanVal(s[i+1]);
+        if(curr<next)
+        result-=curr;
+        else
+        result+=curr;
 
         i++;
     }
-    return res;
+    return result;
 }
