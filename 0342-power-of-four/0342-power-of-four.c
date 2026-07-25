f8bae2aac683fe0f2e14f008695ec18bc6326fdb
Ramesh Pandhare <rameshpandhare347@gmail.com>
2026-07-25 12:27:39 +0530

    Time: 0 ms (100%), Space: 8.4 MB (76.58%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.c
@@ -0,0 +1,11 @@
+bool isPowerOfFour(int n) 
+{
+    if(n<=0) return false;
+    if(n==1) return true;
+    while(n%4==0)
+    {
+        n/=4;
+        if(n==1) return true;
+    }
+    return false;
+}
