a60844815515d053b462dab7e3152bb6c01f3076
Ramesh Pandhare <rameshpandhare347@gmail.com>
2026-08-15 18:55:43 +0530

    Time: 0 ms (100%), Space: 8.4 MB (92.39%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.c
@@ -0,0 +1,11 @@
+int bitwiseComplement(int num)
+{
+    if(num==0) return 1;
+    int i;
+    for(i=31; i>=0; i--)
+    if(((unsigned)num>>i)&1U)
+    break;
+
+    unsigned int mask=(1U<<(i+1))-1;
+    return (num ^ mask); 
+}
