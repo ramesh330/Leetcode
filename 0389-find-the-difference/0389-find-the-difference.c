e8622ee5b83ab8c59efde2ea727950dcabbda355
Ramesh Pandhare <rameshpandhare347@gmail.com>
2026-08-18 14:56:11 +0530

    Time: 0 ms (100%), Space: 8.9 MB (34.2%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.c
+++ 0389-find-the-difference/0389-find-the-difference.c
@@ -1,18 +1,10 @@
 char findTheDifference(char* s, char* t) 
 {
-    if(strlen(s)>=strlen(t))
-    return '\0';
-    
-    int arr[26]={0};
+    int sum1=0,sum2=0;
     for(int i=0; s[i]!='\0'; i++)
-    arr[s[i]-'a']++;
-
+    sum1+=s[i];
     for(int i=0; t[i]!='\0'; i++)
-    {
-        if(arr[t[i]-'a'] == 0)
-        return t[i];
-        else arr[t[i]-'a']--;
-    }
-    return '\0';
+    sum2+=t[i];
+    return sum2-sum1;
 }
     
