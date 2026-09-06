1ae50c3de64a6aa4e9096548474882cb0eb6148f
Ramesh Pandhare <rameshpandhare347@gmail.com>
2026-09-06 18:21:33 +0530

    Time: 0 ms (100%), Space: 45.3 MB (89.74%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.c
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.c
@@ -7,44 +7,38 @@
  */
 bool isPalindrome(struct ListNode* head) 
 {
-    if(head==NULL)
-    return false;
-
-    if(head->next == NULL)
+    if(head==NULL || head->next == NULL)
     return true;
 
-    if(head->next->next==NULL)
+    struct ListNode* fast = head;
+    struct ListNode* slow = head;
+    
+    while(fast && fast->next)
     {
-        if(head->val == head->next->val)
-        return true;
-        return false;
+        fast = fast->next->next;
+        slow = slow->next;
     }
 
-    struct ListNode *temp = head,*temp2 = NULL;
-    struct ListNode *rev = temp2;
+    struct ListNode* curr = slow;
+    struct ListNode* prev = NULL;
+    struct ListNode* nextNode = NULL;
 
-    while(temp)
+    while(curr)
     {
-        struct ListNode *new = malloc(sizeof(struct ListNode));
-        new->val = temp->val;
-        new->next = NULL;
+        nextNode = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextNode;
+    }
 
-        if(rev==NULL)
-        rev = new;
-        else
-        {
-            new->next = rev;
-            rev = new;
-        }
-        temp = temp->next;
-    }  
-    
-    while(rev && head)
+    struct ListNode* first = head,*second = prev;
+    while(second)
     {
-        if(rev->val != head->val)
+        if(first->val != second->val)
         return false;
-        rev = rev->next;
-        head = head->next;
+
+        first = first->next;
+        second = second->next;
     }
     return true;
 }
