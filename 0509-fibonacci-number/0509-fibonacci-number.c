5e5cb4e7722e33b2b9141c4ed06a224f2d83cdf1
Ramesh Pandhare <rameshpandhare347@gmail.com>
2026-09-16 15:07:11 +0530

    Time: 3 ms (66.2%), Space: 8.4 MB (89.87%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.c
+++ 0509-fibonacci-number/0509-fibonacci-number.c
@@ -2,5 +2,11 @@ int fib(int n)
 {
     if(n==0) return 0;
     if(n==1) return 1;
-    return fib(n-1) + fib(n-2);
+    int f=0,s=1,t=0;
+    for(int i=2; i<=n; i++){
+        t = f + s;
+        f = s;
+        s = t;
+    }
+    return t;
 }
