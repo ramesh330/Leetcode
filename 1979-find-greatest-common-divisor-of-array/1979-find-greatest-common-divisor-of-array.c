85e6d4b106417c7f2c94061319462711ee4f3038
Ramesh Pandhare <rameshpandhare347@gmail.com>
2026-09-17 14:19:00 +0530

    Time: 0 ms (100%), Space: 9.2 MB (25.9%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.c
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.c
@@ -1,19 +1,16 @@
 int findGCD(int* nums, int numsSize) 
 {
-    for(int i=0; i<numsSize-1; i++)
-    for(int j=0; j<numsSize-i-1; j++)
+    int s=10001,l=0;
+    for(int i=0; i<numsSize; i++)
     {
-        if(nums[j]>nums[j+1])
-        {
-            int temp = nums[j];
-            nums[j] = nums[j+1];
-            nums[j+1] = temp;
-        }
+        if(nums[i] < s)
+        s = nums[i];
+        if(nums[i] > l)
+        l = nums[i];
     }
-
     int max=1;
     for(int i=2; i<=nums[numsSize-1]; i++)
-    if(nums[0]%i==0 && nums[numsSize-1]%i==0)
+    if(s%i==0 && l%i==0)
     max=i;
     return max;   
 }
